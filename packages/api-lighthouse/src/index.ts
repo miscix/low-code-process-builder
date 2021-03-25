c4ac2d420d02ec9355ed642385ee895e908cccc6
Mushegh Antaranyan <mushegh.codes@gmail.com>
2021-03-25 23:22:26 +0400

    feat(api): implement lighthouse analyses

--- packages/api-lighthouse/src/index.ts
+++ packages/api-lighthouse/src/index.ts
@@ -1,9 +1,42 @@
-import { json } from "micro";
-import { IncomingMessage } from "node:http";
+import { json, sendError } from "micro";
+import { IncomingMessage, ServerResponse } from "node:http";
 
-export default async function (req: IncomingMessage) {
-  const data = await json(req);
-  console.log(data);
+import * as lighthouse from "lighthouse";
 
-  return "ok";
+import { launch } from "chrome-launcher";
+
+type OutputFormat = "json" | "html";
+
+interface InputParams {
+  url: string;
+  format?: OutputFormat;
+}
+
+async function acquireAnalytics(url: string, format: OutputFormat = "json") {
+  const chrome = await launch({ chromeFlags: ["--headless"] });
+  const options = {
+    logLevel: "info",
+    output: "html",
+    onlyCategories: ["performance"],
+    port: chrome.port,
+  };
+  const runnerResult = await lighthouse(url, options);
+
+  await chrome.kill();
+
+  switch (format) {
+    case "json":
+      const { audits, categories } = runnerResult.lhr;
+      return { audits, categories };
+    case "html":
+      return runnerResult.report;
+  }
+}
+
+export default async function (req: IncomingMessage, res: ServerResponse) {
+  const data = (await json(req)) as InputParams;
+
+  return acquireAnalytics(data.url, data.format).catch((err) =>
+    sendError(req, res, { message: err.message })
+  );
 }
